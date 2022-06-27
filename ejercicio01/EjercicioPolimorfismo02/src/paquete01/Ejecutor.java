9c3e4a55d05811f6d7b1bf81ac7726dfe272d9d2
Amonint <payala909@gmail.com>
2022-06-27 13:39:15 -0500

    +++

--- ejercicio01/EjercicioPolimorfismo02/src/paquete01/Ejecutor.java
+++ ejercicio01/EjercicioPolimorfismo02/src/paquete01/Ejecutor.java
@@ -5,12 +5,92 @@
  */
 package paquete01;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import paquete02.Automovil;
+import paquete03.AutomovilGasolina;
+import paquete04.AutomovilDiesel;
+
 /**
  *
  * @author reroes
  */
 public class Ejecutor {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        String nombrespropiet, placa;
+        int edadEst;
+        double nummaxli, costoli,nomP,descuentol;
+        int numeroCreds;
+        int numeroAsigs;
+        int tipoAutom;
+        String continuar;
+        int contador = 0;
+        ArrayList<Automovil> automovils = new ArrayList<>();
+        boolean bandera = true;
+
+        // inicio de solución
+        while (bandera) {
+
+            System.out.println("Tipo de automoviles a ingresar\n"
+                    + "Ingrese (1) para Automovil Gasolina\n"
+                    + "Ingrese (2) Automovil Diesel\n");
+            tipoAutom = Integer.parseInt(entrada.nextLine());
+            System.out.println("Ingrese los nombres del propietario");
+            nombrespropiet = entrada.nextLine();
+            System.out.println("Ingrese la placa");
+            placa = entrada.nextLine();
+
+            if (tipoAutom == 1) {
+                AutomovilGasolina gasolina = new AutomovilGasolina();
+                System.out.println("Ingrese el numero de galones: ");
+                gasolina.establecerNumGalones(entrada.nextDouble());
+                System.out.println("Ingrese el costo de galones: ");
+                gasolina.establecerCostoGalones(entrada.nextDouble());
+                gasolina.establecerNomPro(nombrespropiet);
+                gasolina.establecerIva();
+                gasolina.establecerPlaca(placa);
+
+                automovils.add(gasolina);
+
+            } else {
+
+                AutomovilDiesel diesel = new AutomovilDiesel();
+
+                System.out.println("Ingrese el número de litros maximos del tanque");
+                nummaxli = entrada.nextDouble();
+                System.out.println("Ingrese el costo de cada litro");
+                costoli = entrada.nextDouble();
+                System.out.println("Ingrese el descuento");
+                descuentol = entrada.nextDouble();
+                diesel.establecerNomPro(nombrespropiet);
+                diesel.establecernumeroLimax(nummaxli);
+                diesel.establecercostolitro(costoli);
+                diesel.establecerPlaca(placa);
+                diesel.establecerdescuentol(descuentol);
+                automovils.add(diesel);
+
+            }
+            entrada.nextLine();
+            System.out.println("¿Desea ingresar otro estudiante?");
+            String a = entrada.nextLine();
+            if ("si".equals(a) || "Si".equals(a)) {
+                bandera = true;
+            } else {
+                bandera = false;
+            }
+        }
+
+        for (int i = 0; i < automovils.size(); i++) {
+            // 1.
+            automovils.get(i).calcularValorCancelar();
+
+            System.out.printf("Datos propietario\n"
+                    + "%s\n",
+                    automovils.get(i));
+
+        }
         
     }
 }
--- ejercicio01/EjercicioPolimorfismo02/src/paquete04/AutomovilDiesel.java
+++ ejercicio01/EjercicioPolimorfismo02/src/paquete04/AutomovilDiesel.java
@@ -5,10 +5,67 @@
  */
 package paquete04;
 
+import paquete02.Automovil;
+
 /**
  *
  * @author reroes
  */
-public class AutomovilDiesel {
+public class AutomovilDiesel extends Automovil {
+    /*1.  Declarar
+        numeroAsignaturas: Entero 
+        costoAsginatura: Real 
+    */ 
     
+    double numeroLimax;
+    double costolitro;
+    double descuentol;
+    
+    //  Métodos establecer y calcular para los datos o atributos de la clase
+    
+    // 2.  Método establecerNumeroAsginaturas(numero: Real)
+    public void establecernumeroLimax(double numero){
+        numeroLimax = numero;
+    }
+    
+    // 3.  Método establecerCostoAsignatura(valor: Real)
+    public void establecercostolitro(double valor){
+        costolitro = valor;
+    }
+    public void establecerdescuentol(double desc){
+        descuentol = desc;
+    }
+
+    // 4.  Método calcularMatriculaDistancia()
+    @Override
+    public void calcularValorCancelar(){
+        valorCancelar = numeroLimax-((numeroLimax*descuentol)/100);
+        valorCancelar = valorCancelar*costolitro;
+    }
+
+    //  Métodos obtener para los datos o atributos de la clase
+    // 5. Método obtenerNumeroAsignaturas() : Entero
+    public double obtenernumeroLimax(){
+        return numeroLimax; 
+    }
+
+    // 6. Método obtenerCostoAsignatura() : Real
+    public double obtenercostolitro(){
+        return costolitro;
+    }
+    public double obtenerdescuentol(){
+        return descuentol;
+    }
+    
+    public String toString(){
+        String cadena = String.format("%s\n"
+                        + "Numero Litro: %.2f\n"
+                        + "Costo de Litros: %.2f\n"
+                        + "Descuento: %.2f\n",
+                        super.toString(),
+                        obtenernumeroLimax(),
+                        obtenercostolitro(),
+                        obtenerdescuentol()); 
+        return cadena;
+    }
 }
