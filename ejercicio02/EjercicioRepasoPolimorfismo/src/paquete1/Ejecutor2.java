4895502faa600bf826cc68d3006d9a58a5031ac1
Amonint <payala909@gmail.com>
2022-06-27 14:42:29 -0500

    ++

--- ejercicio02/EjercicioRepasoPolimorfismo/src/paquete1/Ejecutor2.java
+++ ejercicio02/EjercicioRepasoPolimorfismo/src/paquete1/Ejecutor2.java
@@ -30,9 +30,40 @@ public class Ejecutor2 {
         ¿Cómo saber a que tipo de Pasaje pertenece cada objeto? Usar la edad 
         como discriminatoria.
         
+        menor de edad: mayor o igual a 0 y menor o igual a 18
+        universitario: mayor a 18 y menor o igual a 25
+        normal: mayor a 25 y menor 65
+        tercera edad: mayor o igual a 65
+        
         */
         // inicio de solución
-        
+        for (int i = 0; i < nombres.length; i++) {
+            if (edad.length>=0 &&edad.length<=18) {
+                PasajeMenorEdad pasajeMenorEdad = new PasajeMenorEdad(pasajeFijo);
+                Persona pa= new Persona(nombres[i],apellidos[i],edad[i]);
+                pasajeMenorEdad.establecerPersona(pa);
+                pasajes.add(pasajeMenorEdad);
+                // Se agrega al arreglo estudiantes un objeto de tipo
+                // EstudiantePresencial
+//                estudiantes[contador] = estudianteP;
+            }if (edad.length>=18 &&edad.length<=25) {
+                PasajeUniversitario pasajeUni = new PasajeUniversitario(pasajeFijo);
+                Persona pa= new Persona(nombres[i],apellidos[i],edad[i]);
+                pasajeUni.establecerPersona(pa);
+                pasajes.add(pasajeUni);
+            }if (edad.length>=25 &&edad.length<=65) {
+                PasajeNormal pasajeNormal = new PasajeNormal(pasajeFijo);
+                Persona pa= new Persona(nombres[i],apellidos[i],edad[i]);
+                pasajeNormal.establecerPersona(pa);
+                pasajes.add(pasajeNormal);
+            }if (edad.length>=65) {
+                PasajeTerceraEdad pasajeter = new PasajeTerceraEdad(pasajeFijo);
+                Persona pa= new Persona(nombres[i],apellidos[i],edad[i]);
+                pasajeter.establecerPersona(pa);
+                pasajes.add(pasajeter);
+            }
+            
+        }
         
         // fin  de solución
         
